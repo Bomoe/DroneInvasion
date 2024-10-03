4568bd555b607d376dc6fe24af9bff3eecf42884
Bomoe <hughesmoe3@gmail.com>
2024-10-03 00:43:59 -0500

    Adding game over mechanics, lives, and a few other things

--- global_settings.gd
+++ global_settings.gd
@@ -1,3 +1,4 @@
 extends Node
 
-@export var enemy_speed = 25
+@export var max_enemy_speed = 25
+@export var current_enemy_speed = max_enemy_speed
--- player/player.gd
+++ player/player.gd
@@ -1,14 +1,19 @@
 extends Area2D
 
-signal hit
+signal dead
 signal enemy_killed
+signal life_lost
+signal life_add
 
 @onready var Bullet = preload("res://weapons/Bullet.tscn")
 
 @export var speed = 400
+@export var max_health = 3
+var health = max_health
 var can_shoot = true
 var screen_size
 var direction_to_mouse = Vector2.ZERO
+var is_invuln = false
 
 func _ready() -> void:
 	screen_size = get_viewport_rect().size
@@ -49,29 +54,45 @@ func fire_bullet():
 	$FireTimer.start()
 	can_shoot = false
 	var bullet = Bullet.instantiate()
-	bullet.global_position = position + direction_to_mouse * 10
+	bullet.global_position = position + direction_to_mouse * 50
 	bullet.rotation = direction_to_mouse.angle()
 
-    # Add the bullet to the scene tree
+	# Add the bullet to the scene tree
 	get_parent().add_child(bullet)
 
-    # Emit the fire signal on the bullet
+	# Emit the fire signal on the bullet
 	bullet.emit_signal("_on_fire")
 
 func _on_body_entered(body: Node2D) -> void:
-	hide()
-	hit.emit()
-	$CollisionShape2D.set_deferred("disabled", true)
+	if is_invuln:
+		return
+	is_invuln = true
+	$InvulnTimer.start()
+	$AnimatedSprite2D.self_modulate.a = .5
+	if health <= 0:
+		dead.emit()
+		$CollisionShape2D.set_deferred("disabled", true)
+		hide()
+	health -= 1
+	life_lost.emit(health)
+	
 
 func start(pos):
 	position = pos
 	show()
-	print("Player position: ", position)
-	print("Player group:", is_in_group("player"))
 	$CollisionShape2D.disabled = false
+	health = max_health
+	$AnimatedSprite2D.self_modulate.a = 1
+	
 
 func on_fire_timer_reset():
 	can_shoot = true
 
 func _on_enemy_killed():
 	enemy_killed.emit()
+
+
+func _on_invuln_timer_timeout() -> void:
+	is_invuln = false
+	$AnimatedSprite2D.self_modulate.a = 1
+	
